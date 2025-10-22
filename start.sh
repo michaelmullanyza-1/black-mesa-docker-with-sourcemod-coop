6dd1770b78450fd272db29c17e0cbb70aecc4231
michaelmullanyza-1 <michaelmullany.za@gmail.com>
2025-10-22 15:01:23 +0200

    Update start.sh
    Updated to a higher port range: 27315-27316

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-/home/steam/mesa/srcds_run -game bms -secure -port 27016 +clientport 27017 +maxplayers 10 +mp_teamplay 1 +exec server.cfg +map bm_c0a0a
+/home/steam/mesa/srcds_run -game bms -secure -port 27315 +clientport 27316 +maxplayers 10 +mp_teamplay 1 +exec server.cfg +map bm_c0a0a
