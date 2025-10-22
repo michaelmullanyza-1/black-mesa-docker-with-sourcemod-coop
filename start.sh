50dac88490ae0cdeb26a9636ab338d922f0e394f
michaelmullanyza-1 <michaelmullany.za@gmail.com>
2025-10-22 18:34:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -e  # stop on first error
-set -x  # echo commands for debugging
+set -e
 
 STEAMCMD_DIR=/home/steam/steamcmd
 GAME_DIR=/home/steam/mesa
@@ -8,25 +7,20 @@ BMS_DIR=$GAME_DIR/bms
 
 mkdir -p $GAME_DIR
 mkdir -p $BMS_DIR
+mkdir -p $BMS_DIR/cfg
 
-# Step 1: Download/update Black Mesa
+# Download/update Black Mesa
 echo "Updating Black Mesa..."
 $STEAMCMD_DIR/steamcmd.sh +force_install_dir $GAME_DIR +login anonymous +app_update 346680 validate +quit
 
-# Check that srcds_run exists
+# Make sure the server file exists
 if [ ! -f $GAME_DIR/srcds_run ]; then
-    echo "Error: srcds_run not found! Black Mesa did not download correctly."
+    echo "Error: srcds_run not found! Exiting."
     exit 1
 fi
 
-# Step 2: Install mods
-echo "Installing mods..."
-cd $BMS_DIR
-
-# Make sure curl/wget exits on failure
-set -e
-
 # Download mods
+cd $BMS_DIR
 wget -N https://mms.alliedmods.net/mmsdrop/1.12/mmsource-1.12.0-git1156-linux.tar.gz
 tar -xvzf mmsource-1.12.0-git1156-linux.tar.gz
 
@@ -36,10 +30,8 @@ tar -xvzf sourcemod-1.12.0-git7163-linux.tar.gz
 wget -N https://github.com/ampreeT/SourceCoop/releases/download/v1.5-beta2/SourceCoop-1.5-beta2-bms.zip
 unzip -o SourceCoop-1.5-beta2-bms.zip
 
-# Step 3: Copy server.cfg safely
-mkdir -p $BMS_DIR/cfg
+# Copy server.cfg
 cp -f /home/steam/server.cfg $BMS_DIR/cfg/server.cfg
 
-# Step 4: Start server
-echo "Starting Black Mesa server..."
+# Start server
 $GAME_DIR/srcds_run -game bms -secure -port 27315 +clientport 27316 +maxplayers 10 +mp_teamplay 1 +exec server.cfg +map bm_c0a0a
