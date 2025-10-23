843121e04ba41762d307de43b7d844899064f339
michaelmullanyza-1 <michaelmullany.za@gmail.com>
2025-10-23 13:42:48 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 set -e
 
-STEAMCMD_DIR=/home/steam/steamcmd
-GAME_DIR=/home/steam/mesa
-BMS_DIR=$GAME_DIR/bms
+SERVER_DIR="/home/steam/mesa"
+STEAMCMD="/home/steam/steamcmd/steamcmd.sh"
+BMS_APP_ID=346680
 
-mkdir -p $BMS_DIR/cfg
+echo "=== Updating Black Mesa Dedicated Server ==="
+$STEAMCMD +force_install_dir "$SERVER_DIR" +login anonymous +app_update $BMS_APP_ID validate +quit
 
-echo "Updating Black Mesa..."
-$STEAMCMD_DIR/steamcmd.sh +force_install_dir $GAME_DIR +login anonymous +app_update 346680 +quit
+cd "$SERVER_DIR"
 
-echo "Downloading mods..."
-cd $BMS_DIR
+# Ensure folder structure
+mkdir -p "$SERVER_DIR/bms/cfg"
 
-# Download mods if not present
-[ ! -f mmsource-1.12.0-git1156-linux.tar.gz ] && \
-    wget https://mms.alliedmods.net/mmsdrop/1.12/mmsource-1.12.0-git1156-linux.tar.gz
-[ ! -f sourcemod-1.12.0-git7163-linux.tar.gz ] && \
-    wget https://sm.alliedmods.net/smdrop/1.12/sourcemod-1.12.0-git7163-linux.tar.gz
-[ ! -f SourceCoop-1.5-beta2-bms.zip ] && \
-    wget https://github.com/ampreeT/SourceCoop/releases/download/v1.5-beta2/SourceCoop-1.5-beta2-bms.zip
+echo "=== Installing Mods (if missing) ==="
 
-echo "Installing mods..."
-tar -xvzf mmsource-1.12.0-git1156-linux.tar.gz -C $BMS_DIR || true
-tar -xvzf sourcemod-1.12.0-git7163-linux.tar.gz -C $BMS_DIR || true
-unzip -o SourceCoop-1.5-beta2-bms.zip -d $BMS_DIR || true
-
-# Ensure server.cfg is in place
-if [ ! -f $BMS_DIR/cfg/server.cfg ]; then
-    cp $BMS_DIR/cfg/server.cfg.bck $BMS_DIR/cfg/server.cfg 2>/dev/null || true
+# MetaMod
+if [ ! -d "$SERVER_DIR/bms/addons/metamod" ]; then
+  echo "Installing MetaMod..."
+  wget -q https://mms.alliedmods.net/mmsdrop/1.12/mmsource-1.12.0-git1156-linux.tar.gz
+  tar -xzf mmsource-1.12.0-git1156-linux.tar.gz -C "$SERVER_DIR/bms"
+  rm mmsource-1.12.0-git1156-linux.tar.gz
 fi
 
-echo "Starting Black Mesa server..."
-$GAME_DIR/srcds_run -game bms -secure -port 27315 +clientport 27316 +maxplayers 10 +mp_teamplay 1 +exec server.cfg +map bm_c0a0a
+# SourceMod
+if [ ! -d "$SERVER_DIR/bms/addons/sourcemod" ]; then
+  echo "Installing SourceMod..."
+  wget -q https://sm.alliedmods.net/smdrop/1.12/sourcemod-1.12.0-git7163-linux.tar.gz
+  tar -xzf sourcemod-1.12.0-git7163-linux.tar.gz -C "$SERVER_DIR/bms"
+  rm sourcemod-1.12.0-git7163-linux.tar.gz
+fi
+
+# SourceCoop
+if [ ! -d "$SERVER_DIR/bms/addons/sourcemod/plugins/sourcecoop" ]; then
+  echo "Installing SourceCoop..."
+  wget -q https://github.com/Benoist3012/SourceCoop/releases/download/1.5-beta2/SourceCoop-1.5-beta2-bms.zip
+  unzip -o SourceCoop-1.5-beta2-bms.zip -d "$SERVER_DIR/bms"
+  rm SourceCoop-1.5-beta2-bms.zip
+fi
+
+# Copy server config
+cp /home/steam/server.cfg "$SERVER_DIR/bms/cfg/server.cfg"
+
+echo "=== Starting Black Mesa Dedicated Server ==="
+cd "$SERVER_DIR"
+
+# Run the server
+./srcds_run -game bms -secure -port 27015 +clientport 27016 +maxplayers 8 +map bm_c0a0a +exec server.cfg
